457e3ce6e9718432f620f1f887c48582c9759d7f
qingwei.li <cinwell.li@gmail.com>
2016-11-29 19:50:01 +0800

    Fix ineffective option, fixed #10

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const script = document.currentScript || [].slice.call(document.getElementsByTag
 if (script) {
   for (const prop in OPTIONS) {
     const val = script.getAttribute('data-' + camel2kebab(prop))
-    OPTIONS[prop] = isNil(val) ? OPTIONS[prop] : true
+    OPTIONS[prop] = isNil(val) ? OPTIONS[prop] : (val || true)
   }
   if (OPTIONS.loadSidebar === true) OPTIONS.loadSidebar = '_sidebar.md'
   if (OPTIONS.loadNavbar === true) OPTIONS.loadNavbar = '_navbar.md'
