6e81e66441f989d8b86c5c1d43776ec498ad8811
qingwei.li <cinwell.li@gmail.com>
2016-12-24 19:50:41 +0800

    404.dev => dev

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var fs = require('fs')
 
 http.createServer(function (req, res) {
   serveStatic('.')(req, res, function () {
-    res.writeHead(404, { 'Content-Type': 'text/html' })
-    res.end(fs.readFileSync('404.dev.html'))
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(fs.readFileSync('dev.html'))
   })
 }).listen(3000, '0.0.0.0')
 
