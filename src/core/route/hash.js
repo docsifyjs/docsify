a70620387953f8f50bc0e7564610f38c029b76b2
Bojan Djurkovic <dbojan@gmail.com>
2017-03-31 09:08:05 -0300

    add optional current route param to toURL and use it to properly compose local anchor links

--- src/core/route/hash.js
+++ src/core/route/hash.js
@@ -64,16 +64,17 @@ export function parse (path = window.location.href) {
  * to URL
  * @param  {string} path
  * @param  {object} qs   query params
+ * @param  {string} currentRoute optional current route
  */
-export function toURL (path, params) {
-  const inline = path[0] === '#'
+export function toURL (path, params, currentRoute) {
+  const local = currentRoute && path[0] === '#'
   const route = parse(replaceSlug(path))
 
   route.query = merge({}, route.query, params)
   path = route.path + stringifyQuery(route.query)
   path = path.replace(/\.md(\?)|\.md$/, '$1')
 
-  if (inline) path = currentPath + path
+  if (local) path = currentRoute + path
 
   return cleanPath('#/' + path)
 }
