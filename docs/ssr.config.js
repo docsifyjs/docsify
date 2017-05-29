891475e132c3242bf0867b481f30ed1477f7aa80
qingwei.li <cinwell.li@gmail.com>
2017-05-30 03:48:29 +0800

    refactor(ssr): for test

---
+++ docs/ssr.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  alias: {
+    '/de-de/changelog': '/changelog',
+    '/zh-cn/changelog': '/changelog',
+    '/changelog': 'https://raw.githubusercontent.com/QingWei-Li/docsify/master/CHANGELOG'
+  },
+  auto2top: true,
+  coverpage: true,
+  executeScript: true,
+  loadSidebar: true,
+  loadNavbar: true,
+  mergeNavbar: true,
+  maxLevel: 4,
+  subMaxLevel: 2,
+  name: 'docsify',
+  search: {
+    noData: {
+      '/de-de/': 'Keine Ergebnisse!',
+      '/zh-cn/': '没有结果!',
+      '/': 'No results!'
+    },
+    paths: 'auto',
+    placeholder: {
+      '/de-de/': 'Suche',
+      '/zh-cn/': '搜索',
+      '/': 'Search'
+    }
+  }
+}
