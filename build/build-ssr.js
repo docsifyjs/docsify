9825db47af334728da2709d6f371e0736157b099
qingwei.li <cinwell.li@gmail.com>
2017-05-30 13:58:05 +0800

    fix: update babel config

--- build/build-ssr.js
+++ build/build-ssr.js
@@ -1,21 +1,17 @@
 var rollup = require('rollup')
-var babel = require('rollup-plugin-babel')
+var buble = require('rollup-plugin-buble')
+var async = require('rollup-plugin-async')
 var isProd = process.argv[process.argv.length - 1] !== '--dev'
 
 rollup
   .rollup({
     entry: 'packages/docsify-server-renderer/index.js',
     plugins: [
-      babel({
-        plugins: ['transform-async-to-generator'],
-        presets: [
-          [
-            'es2015',
-            {
-              modules: false
-            }
-          ]
-        ]
+      async(),
+      buble({
+        transforms: {
+          generator: false
+        }
       })
     ],
     onwarn: function() {}
