761e38f6adaec4eba7d0f633d9ffc19b40f1da2a
qingwei.li <cinwell.li@gmail.com>
2017-07-24 19:06:24 +0800

    chore: add changelog 4.2.2

--- build/release.sh
+++ build/release.sh
@@ -33,7 +33,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   npm version $VERSION --message "[release] $VERSION $RELEASE_TAG"
 
   # changelog
-  node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s
+  node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s -r 2
 
   git add .
   git commit -m "chore: add changelog $VERSION"
