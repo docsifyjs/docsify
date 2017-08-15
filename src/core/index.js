b7735066ffd70440b4156add749e4d0dea002626
qingwei.li <cinwell.li@gmail.com>
2017-08-15 23:46:47 +0800

    chore(*): remove document-ready

--- src/core/index.js
+++ src/core/index.js
@@ -1,10 +1,22 @@
-import ready from 'document-ready'
 import { initMixin } from './init'
 import { routerMixin } from './router'
 import { renderMixin } from './render'
 import { fetchMixin } from './fetch'
 import { eventMixin } from './event'
 import initGlobalAPI from './global-api'
+
+/**
+ * Fork https://github.com/bendrucker/document-ready/blob/master/index.js
+ */
+function ready (callback) {
+  const state = document.readyState
+
+  if (state === 'complete' || state === 'interactive') {
+    return setTimeout(callback, 0)
+  }
+
+  document.addEventListener('DOMContentLoaded', callback)
+}
 
 function Docsify () {
   this._init()
