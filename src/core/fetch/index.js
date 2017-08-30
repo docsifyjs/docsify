3a97a874a8892fb598313c55174f6c179dd947bb
fy <fy0@qq.com>
2017-08-30 20:48:32 +0800

    sidebar load fix

--- src/core/fetch/index.js
+++ src/core/fetch/index.js
@@ -34,7 +34,7 @@ export function fetchMixin (proto) {
       const fn = result => { this._renderSidebar(result); cb() }
 
       // Load sidebar
-      loadNested('/', loadSidebar, fn, this, true)
+      loadNested(path, loadSidebar, fn, this, true)
     }
 
     // Load main content
