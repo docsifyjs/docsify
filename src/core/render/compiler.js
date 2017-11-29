e43ded4ea7fc48b2ff2adba4e6706b39b3b3e92c
qingwei.li <cinwell.li@gmail.com>
2017-11-29 09:45:40 +0800

    fix(compiler): task lists style, fixed #215

--- src/core/render/compiler.js
+++ src/core/render/compiler.js
@@ -181,7 +181,7 @@ export class Compiler {
       if (checked) {
         text = text.replace(CHECKED_RE, `<input type="checkbox" ${checked[1] === 'x' ? 'checked' : ''} />`)
       }
-      return `<li>${text}</li>\n`
+      return `<li${checked ? ` class="task-list-item"` : ''}>${text}</li>\n`
     }
 
     renderer.origin = origin
