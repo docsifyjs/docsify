95e95bd23b2d4edf9504ede599b59ddbaa63ce01
qingwei.li <cinwell.li@gmail.com>
2018-03-03 23:19:46 +0800

    chore: remove fuck cssnano-cli

---
+++ build/mincss.js
@@ -0,0 +1,12 @@
+const cssnano = require('cssnano').process
+const path = require('path')
+const fs = require('fs')
+
+files = fs.readdirSync(path.resolve('lib/themes'))
+
+files.forEach(file => {
+  file = path.resolve('lib/themes', file)
+  cssnano(fs.readFileSync(file)).then(result => {
+    fs.writeFileSync(file, result.css)
+  })
+})
