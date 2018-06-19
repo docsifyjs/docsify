d79613f3def409cdd22bc444f9b733de2dc3fc77
qingwei.li <cinwell.li@gmail.com>
2018-06-19 10:19:00 +0800

    feat: support image resizing, resolve #508

--- src/core/render/compiler.js
+++ src/core/render/compiler.js
@@ -257,6 +257,16 @@ export class Compiler {
         attrs += ` title="${title}"`
       }
 
+      const size = config.size
+      if (size) {
+        const sizes = size.split('x')
+        if (sizes[1]) {
+          attrs += 'width=' + sizes[0] + ' height=' + sizes[1]
+        } else {
+          attrs += 'width=' + sizes[0]
+        }
+      }
+
       if (!isAbsolutePath(href)) {
         url = getPath(contentBase, getParentPath(router.getCurrentPath()), href)
       }
