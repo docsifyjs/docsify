85fcb3f2a1f03544423417a0ddf7c0f27a208ed6
John Hildenbiddle <john@hildenbiddle.com>
2018-10-09 14:46:35 -0700

    Fix zoom-image target elements
    - Ignore images outside of main content area (e.g. logo, sidenav icons, etc.)
    - Ignore linked images (previous behavior broke linked images)
    
    Fixes #520

--- src/plugins/zoom-image.js
+++ src/plugins/zoom-image.js
@@ -1,14 +1,20 @@
 import mediumZoom from 'medium-zoom'
 
+const matchesSelector = Element.prototype.matches || Element.prototype.webkitMatchesSelector || Element.prototype.msMatchesSelector
+
 function install(hook) {
   let zoom
 
   hook.doneEach(_ => {
+    let elms = Array.apply(null, document.querySelectorAll('.markdown-section img:not(.emoji):not([data-no-zoom])'))
+
+    elms = elms.filter(elm => matchesSelector.call(elm, 'a img') === false)
+
     if (zoom) {
       zoom.detach()
     }
 
-    zoom = mediumZoom('img:not(.emoji):not([data-no-zoom])')
+    zoom = mediumZoom(elms)
   })
 }
 
