517f0d7dead27481da7eb0707217cf28ffd8b6f4
koooooo-7 <369491420@qq.com>
2020-04-13 23:04:34 +0800

    [fix #1115] Image resize in % doesn't work.

--- src/core/render/utils.js
+++ src/core/render/utils.js
@@ -5,7 +5,7 @@ export function getAndRemoveConfig(str = '') {
     str = str
       .replace(/^'/, '')
       .replace(/'$/, '')
-      .replace(/(?:^|\s):([\w-]+:?)=?([\w-]+)?/g, (m, key, value) => {
+      .replace(/(?:^|\s):([\w-]+:?)=?([\w-%]+)?/g, (m, key, value) => {
         if (key.indexOf(':') === -1) {
           config[key] = (value && value.replace(/&quot;/g, '')) || true;
           return '';
