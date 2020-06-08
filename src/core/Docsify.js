4e6294a75ce99660a24a2055941f9c8c6da435ec
Joe Pea <joe@trusktr.io>
2020-06-07 20:55:22 -0700

    initialize globals in the index file, so the Docsify.js file only exports the Docsify class

--- src/core/Docsify.js
+++ src/core/Docsify.js
@@ -6,7 +6,6 @@ import { routerMixin } from './router';
 import { renderMixin } from './render';
 import { fetchMixin } from './fetch';
 import { eventMixin } from './event';
-import initGlobalAPI from './global-api';
 
 export class Docsify extends multiple(
   initMixin,
@@ -17,8 +16,3 @@ export class Docsify extends multiple(
   fetchMixin,
   eventMixin
 ) {}
-
-/**
- * Global API
- */
-initGlobalAPI();
--- src/core/index.js
+++ src/core/index.js
@@ -1,5 +1,8 @@
 import { documentReady } from './util/dom';
 import { Docsify } from './Docsify';
+import initGlobalAPI from './global-api';
+
+initGlobalAPI();
 
 /**
  * Run Docsify
