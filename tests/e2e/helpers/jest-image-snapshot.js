8f21d5e11e35c04a6f9b0396d09dce12e4ce4b12
John Hildenbiddle <john@hildenbiddle.com>
2020-07-23 12:55:12 -0400

    Store pixel and ssim comparison configs

--- tests/e2e/helpers/jest-image-snapshot.js
+++ tests/e2e/helpers/jest-image-snapshot.js
@@ -1,14 +1,31 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 
+const config = {
+  // Pixel-based image comparisons
+  // https://github.com/mapbox/pixelmatch#pixelmatchimg1-img2-output-width-height-options
+  pixelCompare: {
+    customDiffConfig: {
+      threshold: 0.3,
+    },
+    failureThreshold: 0.04,
+  },
+  // Structural Similarity Index Measure (SSIM) comparisons
+  // https://github.com/obartra/ssim
+  ssimCompare: {
+    comparisonMethod: 'ssim',
+    failureThreshold: 0.15,
+  },
+};
+
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
-  comparisonMethod: 'ssim',
   customSnapshotIdentifier(data) {
     return `${data.defaultIdentifier}-${browserName}`;
   },
   diffDirection: 'vertical',
-  failureThreshold: 0.15,
   failureThresholdType: 'percent',
   noColors: true,
+  // pixel or ssim
+  ...config.ssimCompare,
 });
 
 expect.extend({ toMatchImageSnapshot });
