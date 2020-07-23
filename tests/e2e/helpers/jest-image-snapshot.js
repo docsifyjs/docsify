dfa5b54652c3957b38f6bd355498f70d350c583f
John Hildenbiddle <john@hildenbiddle.com>
2020-07-23 01:15:58 -0400

    Adjust image snapshot threshold for local & CI tests

--- tests/e2e/helpers/jest-image-snapshot.js
+++ tests/e2e/helpers/jest-image-snapshot.js
@@ -1,6 +1,9 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  customDiffConfig: {
+    threshold: 0.1,
+  },
   customSnapshotIdentifier(data) {
     return `${data.defaultIdentifier}-${browserName}`;
   },
