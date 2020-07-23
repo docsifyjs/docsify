07ef151ba39835d432470ec92936303659f9e10c
John Hildenbiddle <john@hildenbiddle.com>
2020-07-23 12:11:09 -0400

    Bump image snapshot failureThreshold

--- tests/e2e/helpers/jest-image-snapshot.js
+++ tests/e2e/helpers/jest-image-snapshot.js
@@ -6,7 +6,7 @@ const toMatchImageSnapshot = configureToMatchImageSnapshot({
     return `${data.defaultIdentifier}-${browserName}`;
   },
   diffDirection: 'vertical',
-  failureThreshold: 0.1,
+  failureThreshold: 0.15,
   failureThresholdType: 'percent',
   noColors: true,
 });
