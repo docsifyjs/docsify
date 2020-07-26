7a3e7f045c7f1818817d972dc6542e1be5ba4e99
Alex Fang <deeno@qq.com>
2020-07-26 21:29:49 +0800

    Fix sub-sidebar rendering w/ empty level config

--- src/core/render/compiler.js
+++ src/core/render/compiler.js
@@ -293,7 +293,7 @@ export class Compiler {
   subSidebar(level) {
     if (!level) {
       this.toc = [];
-      return;
+      return treeTpl();
     }
 
     const currentPath = this.router.getCurrentPath();
