fd2e16f8eb8b41c641545c57e8191d31d89ef915
Alex Fang <deeno@qq.com>
2020-07-26 18:39:52 +0800

    Remove `location` usage from `isExternal` in `docsify-server-renderer`. - For SSR, there is no location object in Node.js

--- packages/docsify-server-renderer/index.js
+++ packages/docsify-server-renderer/index.js
@@ -20,20 +20,13 @@ function isExternal(url) {
   );
   if (
     typeof match[1] === 'string' &&
-    match[1].length > 0 &&
-    match[1].toLowerCase() !== location.protocol
+    match[1].length > 0
   ) {
     return true;
   }
   if (
     typeof match[2] === 'string' &&
-    match[2].length > 0 &&
-    match[2].replace(
-      new RegExp(
-        ':(' + { 'http:': 80, 'https:': 443 }[location.protocol] + ')?$'
-      ),
-      ''
-    ) !== location.host
+    match[2].length > 0
   ) {
     return true;
   }
