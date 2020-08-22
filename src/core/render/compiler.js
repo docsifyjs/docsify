ce316075e033afdbeb43ce01e284a29fe1870e38
沈唁 <52o@qq52o.cn>
2020-08-22 19:24:56 +0800

    fix : add patch for {docsify-ignore} and {docsify-ignore-all} (#1351)
    * revert: Convert {docsify-ignore} and {docsify-ignore-all} to HTML comments
    
    This reverts commit 90d283d340502456a5d8495df596bb4a02ceb39b
    
    * fix: patch for docsify-ignore
    
    * fix test
    
    * fix test

--- src/core/render/compiler.js
+++ src/core/render/compiler.js
@@ -214,8 +214,20 @@ export class Compiler {
         nextToc.ignoreSubHeading = true;
       }
 
+      if (/{docsify-ignore}/g.test(str)) {
+        str = str.replace('{docsify-ignore}', '');
+        nextToc.title = str;
+        nextToc.ignoreSubHeading = true;
+      }
+
       if (/<!-- {docsify-ignore-all} -->/g.test(str)) {
         str = str.replace('<!-- {docsify-ignore-all} -->', '');
+        nextToc.title = str;
+        nextToc.ignoreAllSubs = true;
+      }
+
+      if (/{docsify-ignore-all}/g.test(str)) {
+        str = str.replace('{docsify-ignore-all}', '');
         nextToc.title = str;
         nextToc.ignoreAllSubs = true;
       }
