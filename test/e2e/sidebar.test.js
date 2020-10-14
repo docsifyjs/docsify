29e3454049e0f52d9a0fc7d4374c72bd3b9eaa59
sy-records <52o@qq52o.cn>
2020-10-14 10:01:05 +0800

    add test

---
+++ test/e2e/sidebar.test.js
@@ -0,0 +1,16 @@
+const docsifyInit = require('../helpers/docsify-init');
+
+// Suite
+// -----------------------------------------------------------------------------
+describe('Sidebar Tests', function() {
+  // Tests
+  // ---------------------------------------------------------------------------
+  test('search readme', async () => {
+    await docsifyInit();
+    await page.goto(DOCS_URL + '/#/quickstart');
+    await page.fill('input[type=search]', 'site generator');
+    expect(
+      await page.innerText('.results-panel > .matching-post > a > h2')
+    ).toEqual('docsify');
+  });
+});
