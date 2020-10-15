dbe002c1da7b83b00916970b8f510fbd7f190728
sy-records <52o@qq52o.cn>
2020-10-15 12:18:55 +0800

    fix test

--- test/e2e/sidebar.test.js
+++ test/e2e/sidebar.test.js
@@ -1,12 +1,9 @@
-const docsifyInit = require('../helpers/docsify-init');
-
 // Suite
 // -----------------------------------------------------------------------------
 describe('Sidebar Tests', function() {
   // Tests
   // ---------------------------------------------------------------------------
   test('search readme', async () => {
-    await docsifyInit();
     await page.goto(DOCS_URL + '/#/quickstart');
     await page.fill('input[type=search]', 'Donate');
     expect(
