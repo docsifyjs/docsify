9847f69f6be647673be2f3c0c2cc43beba42f49f
Joe Pea <joe@trusktr.io>
2020-10-31 12:27:53 -0700

    We don't need to init if we're only testing marked. We probably don't need to test marked either, as we assume that the marked library works as advertised in whichever way they tested it.

--- test/integration/render.test.js
+++ test/integration/render.test.js
@@ -1,20 +1,21 @@
 const stripIndent = require('common-tags/lib/stripIndent');
-const docsifyInit = require('../helpers/docsify-init');
+// const docsifyInit = require('../helpers/docsify-init');
+const marked = require('marked');
 
 // Suite
 // -----------------------------------------------------------------------------
 describe('render', function() {
   // Setup & Teardown
   // -------------------------------------------------------------------------
-  beforeEach(async () => {
-    await docsifyInit();
-  });
+  // beforeEach(async () => {
+  //   await docsifyInit();
+  // });
 
   // Helpers
   // ---------------------------------------------------------------------------
   describe('helpers', () => {
     test('important content', () => {
-      const output = window.marked('!> Important content');
+      const output = marked('!> Important content');
 
       expect(output).toMatchInlineSnapshot(
         `"<p class=\\"tip\\">Important content</p>"`
@@ -22,7 +23,7 @@ describe('render', function() {
     });
 
     test('general tip', () => {
-      const output = window.marked('?> General tip');
+      const output = marked('?> General tip');
 
       expect(output).toMatchInlineSnapshot(
         `"<p class=\\"warn\\">General tip</p>"`
@@ -34,7 +35,7 @@ describe('render', function() {
   // ---------------------------------------------------------------------------
   describe('lists', function() {
     test('as unordered task list', async function() {
-      const output = window.marked(stripIndent`
+      const output = marked(stripIndent`
         - [x] Task 1
         - [ ] Task 2
         - [ ] Task 3
@@ -46,7 +47,7 @@ describe('render', function() {
     });
 
     test('as ordered task list', async function() {
-      const output = window.marked(stripIndent`
+      const output = marked(stripIndent`
         1. [ ] Task 1
         2. [x] Task 2
       `);
@@ -57,7 +58,7 @@ describe('render', function() {
     });
 
     test('normal unordered', async function() {
-      const output = window.marked(stripIndent`
+      const output = marked(stripIndent`
         - [linktext](link)
         - just text
       `);
@@ -68,7 +69,7 @@ describe('render', function() {
     });
 
     test('unordered with custom start', async function() {
-      const output = window.marked(stripIndent`
+      const output = marked(stripIndent`
         1. first
         2. second
 
@@ -83,7 +84,7 @@ describe('render', function() {
     });
 
     test('nested', async function() {
-      const output = window.marked(stripIndent`
+      const output = marked(stripIndent`
         - 1
         - 2
           - 2 a
@@ -101,7 +102,7 @@ describe('render', function() {
   // ---------------------------------------------------------------------------
   describe('images', function() {
     test('regular', async function() {
-      const output = window.marked('![alt text](http://imageUrl)');
+      const output = marked('![alt text](http://imageUrl)');
 
       expect(output).toMatchInlineSnapshot(
         `"<p><img src=\\"http://imageUrl\\" data-origin=\\"http://imageUrl\\" alt=\\"alt text\\"></p>"`
@@ -109,7 +110,7 @@ describe('render', function() {
     });
 
     test('class', async function() {
-      const output = window.marked(
+      const output = marked(
         "![alt text](http://imageUrl ':class=someCssClass')"
       );
 
@@ -119,9 +120,7 @@ describe('render', function() {
     });
 
     test('id', async function() {
-      const output = window.marked(
-        "![alt text](http://imageUrl ':id=someCssID')"
-      );
+      const output = marked("![alt text](http://imageUrl ':id=someCssID')");
 
       expect(output).toMatchInlineSnapshot(
         `"<p><img src=\\"http://imageUrl\\" data-origin=\\"http://imageUrl\\" alt=\\"alt text\\" id=\\"someCssID\\" /></p>"`
@@ -129,7 +128,7 @@ describe('render', function() {
     });
 
     test('no-zoom', async function() {
-      const output = window.marked("![alt text](http://imageUrl ':no-zoom')");
+      const output = marked("![alt text](http://imageUrl ':no-zoom')");
 
       expect(output).toMatchInlineSnapshot(
         `"<p><img src=\\"http://imageUrl\\" data-origin=\\"http://imageUrl\\" alt=\\"alt text\\" data-no-zoom /></p>"`
@@ -138,7 +137,7 @@ describe('render', function() {
 
     describe('size', function() {
       test('width and height', async function() {
-        const output = window.marked(
+        const output = marked(
           "![alt text](http://imageUrl ':size=WIDTHxHEIGHT')"
         );
 
@@ -148,7 +147,7 @@ describe('render', function() {
       });
 
       test('width', async function() {
-        const output = window.marked("![alt text](http://imageUrl ':size=50')");
+        const output = marked("![alt text](http://imageUrl ':size=50')");
 
         expect(output).toMatchInlineSnapshot(
           `"<p><img src=\\"http://imageUrl\\" data-origin=\\"http://imageUrl\\" alt=\\"alt text\\" width=\\"50\\" /></p>"`
@@ -161,7 +160,7 @@ describe('render', function() {
   // ---------------------------------------------------------------------------
   describe('headings', function() {
     test('h1', async function() {
-      const output = window.marked('# h1 tag');
+      const output = marked('# h1 tag');
 
       expect(output).toMatchInlineSnapshot(
         `"<h1 id=\\"h1-tag\\"><a href=\\"#/?id=h1-tag\\" data-id=\\"h1-tag\\" class=\\"anchor\\"><span>h1 tag</span></a></h1>"`
@@ -169,7 +168,7 @@ describe('render', function() {
     });
 
     test('h2', async function() {
-      const output = window.marked('## h2 tag');
+      const output = marked('## h2 tag');
 
       expect(output).toMatchInlineSnapshot(
         `"<h2 id=\\"h2-tag\\"><a href=\\"#/?id=h2-tag\\" data-id=\\"h2-tag\\" class=\\"anchor\\"><span>h2 tag</span></a></h2>"`
@@ -177,7 +176,7 @@ describe('render', function() {
     });
 
     test('h3', async function() {
-      const output = window.marked('### h3 tag');
+      const output = marked('### h3 tag');
 
       expect(output).toMatchInlineSnapshot(
         `"<h3 id=\\"h3-tag\\"><a href=\\"#/?id=h3-tag\\" data-id=\\"h3-tag\\" class=\\"anchor\\"><span>h3 tag</span></a></h3>"`
@@ -185,7 +184,7 @@ describe('render', function() {
     });
 
     test('h4', async function() {
-      const output = window.marked('#### h4 tag');
+      const output = marked('#### h4 tag');
 
       expect(output).toMatchInlineSnapshot(
         `"<h4 id=\\"h4-tag\\"><a href=\\"#/?id=h4-tag\\" data-id=\\"h4-tag\\" class=\\"anchor\\"><span>h4 tag</span></a></h4>"`
@@ -193,7 +192,7 @@ describe('render', function() {
     });
 
     test('h5', async function() {
-      const output = window.marked('##### h5 tag');
+      const output = marked('##### h5 tag');
 
       expect(output).toMatchInlineSnapshot(
         `"<h5 id=\\"h5-tag\\"><a href=\\"#/?id=h5-tag\\" data-id=\\"h5-tag\\" class=\\"anchor\\"><span>h5 tag</span></a></h5>"`
@@ -201,7 +200,7 @@ describe('render', function() {
     });
 
     test('h6', async function() {
-      const output = window.marked('###### h6 tag');
+      const output = marked('###### h6 tag');
 
       expect(output).toMatchInlineSnapshot(
         `"<h6 id=\\"h6-tag\\"><a href=\\"#/?id=h6-tag\\" data-id=\\"h6-tag\\" class=\\"anchor\\"><span>h6 tag</span></a></h6>"`
@@ -211,7 +210,7 @@ describe('render', function() {
 
   describe('link', function() {
     test('regular', async function() {
-      const output = window.marked('[alt text](http://url)');
+      const output = marked('[alt text](http://url)');
 
       expect(output).toMatchInlineSnapshot(
         `"<p><a href=\\"http://url\\" target=\\"_blank\\"  rel=\\"noopener\\">alt text</a></p>"`
@@ -223,7 +222,7 @@ describe('render', function() {
       //   externalLinkTarget: '_blank',
       //   externalLinkRel: 'noopener',
       // });
-      const output = window.marked('[alt text](http://www.example.com)');
+      const output = marked('[alt text](http://www.example.com)');
 
       expect(output).toMatchInlineSnapshot(
         `"<p><a href=\\"http://www.example.com\\" target=\\"_blank\\"  rel=\\"noopener\\">alt text</a></p>"`
@@ -231,7 +230,7 @@ describe('render', function() {
     });
 
     test('disabled', async function() {
-      const output = window.marked("[alt text](http://url ':disabled')");
+      const output = marked("[alt text](http://url ':disabled')");
 
       expect(output).toMatchInlineSnapshot(
         `"<p><a href=\\"javascript:void(0)\\" target=\\"_blank\\"  rel=\\"noopener\\" disabled>alt text</a></p>"`
@@ -239,7 +238,7 @@ describe('render', function() {
     });
 
     test('target', async function() {
-      const output = window.marked("[alt text](http://url ':target=_self')");
+      const output = marked("[alt text](http://url ':target=_self')");
 
       expect(output).toMatchInlineSnapshot(
         `"<p><a href=\\"http://url\\" target=\\"_self\\" >alt text</a></p>"`
@@ -247,9 +246,7 @@ describe('render', function() {
     });
 
     test('class', async function() {
-      const output = window.marked(
-        "[alt text](http://url ':class=someCssClass')"
-      );
+      const output = marked("[alt text](http://url ':class=someCssClass')");
 
       expect(output).toMatchInlineSnapshot(
         `"<p><a href=\\"http://url\\" target=\\"_blank\\"  rel=\\"noopener\\" class=\\"someCssClass\\">alt text</a></p>"`
@@ -257,7 +254,7 @@ describe('render', function() {
     });
 
     test('id', async function() {
-      const output = window.marked("[alt text](http://url ':id=someCssID')");
+      const output = marked("[alt text](http://url ':id=someCssID')");
 
       expect(output).toMatchInlineSnapshot(
         `"<p><a href=\\"http://url\\" target=\\"_blank\\"  rel=\\"noopener\\" id=\\"someCssID\\">alt text</a></p>"`
