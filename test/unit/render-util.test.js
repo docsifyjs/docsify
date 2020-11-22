8ccc20225104376af2e5df6757c4dbd58c0e758e
沈唁 <52o@qq52o.cn>
2020-11-23 06:23:36 +0800

    fix: title error when sidebar link exists with html tag (#1404)
    * fix: title error when sidebar link exists with image
    
    * fix #1408
    
    * add test
    
    * Update

--- test/unit/render-util.test.js
+++ test/unit/render-util.test.js
@@ -1,4 +1,5 @@
 const { removeAtag } = require(`${SRC_PATH}/core/render/utils`);
+const { tree } = require(`${SRC_PATH}/core/render/tpl`);
 
 // Suite
 // -----------------------------------------------------------------------------
@@ -11,5 +12,32 @@ describe('core/render/utils', () => {
 
       expect(result).toEqual('content');
     });
+  });
+});
+
+describe('core/render/tpl', () => {
+  test('remove html tag in tree', () => {
+    const result = tree([
+      {
+        level: 2,
+        slug: '#/cover?id=basic-usage',
+        title: '<span style="color:red">Basic usage</span>',
+      },
+      {
+        level: 2,
+        slug: '#/cover?id=custom-background',
+        title: 'Custom background',
+      },
+      {
+        level: 2,
+        slug: '#/cover?id=test',
+        title:
+          '<img src="/docs/_media/favicon.ico" data-origin="/_media/favicon.ico" alt="ico">Test',
+      },
+    ]);
+
+    expect(result).toEqual(
+      `<ul class="app-sub-sidebar"><li><a class="section-link" href="#/cover?id=basic-usage" title="Basic usage"><span style="color:red">Basic usage</span></a></li><li><a class="section-link" href="#/cover?id=custom-background" title="Custom background">Custom background</a></li><li><a class="section-link" href="#/cover?id=test" title="Test"><img src="/docs/_media/favicon.ico" data-origin="/_media/favicon.ico" alt="ico">Test</a></li></ul>`
+    );
   });
 });
