5e4c79011d1f4ea969879624b234be32d4758d03
Ricardo Gladwell <ricardo@gladwell.me>
2021-02-05 11:55:40 +0000

    Verify sidebar links use file hosting

--- test/e2e/index-file.test.js
+++ test/e2e/index-file.test.js
@@ -1,18 +1,28 @@
 const docsifyInit = require('../helpers/docsify-init');
 
 describe(`Index file hosting`, function() {
-  const sharedConfig = {
-    basePath: `${TEST_HOST}/docs/index.html#/`,
+  const sharedOptions = {
+    config: {
+      basePath: `${TEST_HOST}/docs/index.html#/`,
+    },
+    testURL: `${TEST_HOST}/docs/index.html#/`,
   };
 
   test('should serve from index file', async () => {
-    await docsifyInit({
-      config: sharedConfig,
-    });
+    await docsifyInit(sharedOptions);
 
     await expect(page).toHaveText(
       '#main',
       'A magical documentation site generator'
     );
+    expect(page.url()).toMatch(/index\.html#\/$/);
+  });
+
+  test('should use index file links in sidebar from index file hosting', async () => {
+    await docsifyInit(sharedOptions);
+
+    await page.click('a[href="#/quickstart"]');
+    await expect(page).toHaveText('#main', 'Quick start');
+    expect(page.url()).toMatch(/index\.html#\/quickstart$/);
   });
 });
