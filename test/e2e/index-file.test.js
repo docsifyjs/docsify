f39e07c7b2da951b46460a3ae360631f440eb112
Ricardo Gladwell <ricardo@gladwell.me>
2021-02-05 11:17:54 +0000

    Add e2e test for new Jest test framework

--- test/e2e/index-file.test.js
+++ test/e2e/index-file.test.js
@@ -2,7 +2,7 @@ const docsifyInit = require('../helpers/docsify-init');
 
 describe(`Index file hosting`, function() {
   const sharedConfig = {
-    basePath: `${TEST_HOST}/docs/index.html`,
+    basePath: `${TEST_HOST}/docs/index.html#/`,
   };
 
   test('should serve from index file', async () => {
