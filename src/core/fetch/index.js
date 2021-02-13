e93a26aeb96a7b6ded05e3bf352a5252db393c8c
Giulia <53873910+GiuliCler@users.noreply.github.com>
2021-02-13 12:06:48 +0100

    fix on isExternal function

--- src/core/fetch/index.js
+++ src/core/fetch/index.js
@@ -26,14 +26,14 @@ function isExternal(url) {
   );
   if (
     typeof match[1] === 'string' &&
-    match[1].length > 0 &&
+    match[1].length >= 0 &&
     match[1].toLowerCase() !== location.protocol
   ) {
     return true;
   }
   if (
     typeof match[2] === 'string' &&
-    match[2].length > 0 &&
+    match[2].length >= 0 &&
     match[2].replace(
       new RegExp(
         ':(' + { 'http:': 80, 'https:': 443 }[location.protocol] + ')?$'
