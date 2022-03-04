9d522d5cb8c134632833f79c94d0adb41970f0a8
John Hildenbiddle <jhildenbiddle@users.noreply.github.com>
2022-03-04 17:36:48 -0600

    Fix codesandbox uglify error

--- src/plugins/emoji.js
+++ src/plugins/emoji.js
@@ -1,4 +1,18 @@
+import emojiData from '../core/render/emojify-data';
+
 // Deprecation notice
 if (window && window.console) {
   console.info('Docsify emoji plugin has been deprecated as of v4.13');
 }
+
+// Emoji from GitHub API
+// https://api.github.com/emojis
+window.emojify = function (match, $1) {
+  return Object.prototype.hasOwnProperty.call(emojiData, $1) === false
+    ? match
+    : '<img class="emoji" src="https://github.githubassets.com/images/icons/emoji/' +
+        emojiData[$1] +
+        '.png" alt="' +
+        $1 +
+        '" />';
+};
