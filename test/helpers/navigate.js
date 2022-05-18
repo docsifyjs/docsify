a022b3d292001b3be89e6953c850b992f375021f
Roy Sommer <royso@wix.com>
2022-05-19 00:32:30 +0300

    navigate now supports both hash and history routerModes

--- test/helpers/navigate.js
+++ test/helpers/navigate.js
@@ -1,12 +1,17 @@
 /**
- * Navigate to a specific hashtag route in the page, and wait for docsify to handle navigation.
- * @param {import('playwright-core').Page} page
- * @param {string} route
+ * Navigate to a specific route in the site
+ * @param {import('playwright-core').Page} page the playwright page instance from the test
+ * @param {string} route the route you want to navigate to
+ * @param {Object} opts additional options (optional)
+ * @param {'hash' | 'history'} opts.routerMode which router mode to use. Defaults to "hash"
  */
-async function navigateToRoute(page, route) {
-  await page.evaluate(r => (window.location.hash = r), route);
-  const mainElm = await page.waitForSelector('#main');
-  await mainElm.waitForElementState('stable');
+
+async function navigateToRoute(page, route, { routerMode = 'hash' } = {}) {
+  if (routerMode === 'hash') {
+    await page.evaluate(r => (window.location.hash = r), route);
+  } else {
+    await page.evaluate(r => window.history.pushState({ key: r }, '', r));
+  }
 }
 
 module.exports.navigateToRoute = navigateToRoute;
