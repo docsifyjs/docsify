a764b02156150878c21d978c2ec53f131ddd7579
Jacco-V <info@netsilik.nl>
2022-06-23 11:31:12 +0200

    Push both title and label to toc (=table of contents) array

--- src/core/render/compiler.js
+++ src/core/render/compiler.js
@@ -229,14 +229,14 @@ export class Compiler {
         nextToc.ignoreAllSubs = true;
       }
 
-      const title = config.sidebar || str;
       const slug = slugify(config.id || str);
       const url = router.toURL(router.getCurrentPath(), { id: slug });
-      nextToc.title = removeAtag(title);
+      nextToc.label = removeAtag(config.sidebar || str);
+      nextToc.title = removeAtag(str);
       nextToc.slug = url;
       _self.toc.push(nextToc);
 
-      return `<h${level} id="${slug}"><a href="${url}" data-id="${slug}" class="anchor"><span>${title}</span></a></h${level}>`;
+      return `<h${level} id="${slug}"><a href="${url}" data-id="${slug}" class="anchor"><span>${str}</span></a></h${level}>`;
     };
 
     origin.code = highlightCodeCompiler({ renderer });
