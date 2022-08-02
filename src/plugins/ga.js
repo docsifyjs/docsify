449d00a783eae7416c80da38f9faf1cd8f385ad6
John Hildenbiddle <john@hildenbiddle.com>
2022-08-02 13:21:26 -0500

    test: IIFE (ga & gtag)

--- src/plugins/ga.js
+++ src/plugins/ga.js
@@ -1,40 +1,44 @@
-/* eslint-disable no-console */
-// From https://github.com/egoist/vue-ga/blob/master/src/index.js
-function appendScript() {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.google-analytics.com/analytics.js';
-  document.body.appendChild(script);
-}
-
-function init(id) {
-  appendScript();
-  window.ga =
-    window.ga ||
-    function () {
-      (window.ga.q = window.ga.q || []).push(arguments);
-    };
-
-  window.ga.l = Number(new Date());
-  window.ga('create', id, 'auto');
-}
-
-function collect() {
-  if (!window.ga) {
-    init($docsify.ga);
+(function () {
+  // From https://github.com/egoist/vue-ga/blob/master/src/index.js
+  function appendScript() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.google-analytics.com/analytics.js';
+    document.body.appendChild(script);
   }
 
-  window.ga('set', 'page', location.hash);
-  window.ga('send', 'pageview');
-}
+  function init(id) {
+    appendScript();
+    window.ga =
+      window.ga ||
+      function () {
+        (window.ga.q = window.ga.q || []).push(arguments);
+      };
 
-const install = function (hook) {
-  if (!$docsify.ga) {
-    console.error('[Docsify] ga is required.');
-    return;
+    window.ga.l = Number(new Date());
+    window.ga('create', id, 'auto');
   }
 
-  hook.beforeEach(collect);
-};
+  function collect() {
+    if (!window.ga) {
+      init($docsify.ga);
+    }
 
-$docsify.plugins = [].concat(install, $docsify.plugins);
+    window.ga('set', 'page', location.hash);
+    window.ga('send', 'pageview');
+  }
+
+  const gaPlugin = function (hook, vm) {
+    if (!$docsify.ga) {
+      /* eslint-disable-next-line no-console */
+      console.error('[Docsify] ga is required.');
+      return;
+    }
+
+    hook.beforeEach(collect);
+  };
+
+  // Add plugin to docsify's plugin array
+  $docsify = $docsify || {};
+  $docsify.plugins = [].concat($docsify.plugins || [], gaPlugin);
+})();
