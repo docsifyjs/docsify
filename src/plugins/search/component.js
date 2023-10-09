165c19b30896ddbbed6b06fd5dc05349093220d3
John Hildenbiddle <john@hildenbiddle.com>
2023-10-09 09:24:58 -0500

    Update search field aria label

--- src/plugins/search/component.js
+++ src/plugins/search/component.js
@@ -113,7 +113,7 @@ function style() {
 function tpl(defaultValue = '') {
   const html = /* html */ `
     <div class="input-wrap">
-      <input type="search" value="${defaultValue}" aria-label="Search text" />
+      <input type="search" value="${defaultValue}" aria-label="Search" />
       <div class="clear-button">
         <svg width="26" height="24">
           <circle cx="12" cy="12" r="11" fill="#ccc" />
