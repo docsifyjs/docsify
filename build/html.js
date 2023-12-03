adad7b32413fcd24bb1ec8982cc829918d5dd571
John Hildenbiddle <john@hildenbiddle.com>
2023-12-03 10:17:32 -0600

    Preview fix test (continued)

--- build/html.js
+++ build/html.js
@@ -11,11 +11,12 @@ function generatePreview() {
   const file = 'index.html';
   const srcPath = path.resolve(__dirname, '..', 'docs');
   const srcHTML = fs.readFileSync(path.resolve(srcPath, file), 'utf8');
-  const outPath = path.resolve(__dirname, '..', '.vercel', 'output', 'static');
-  const outHTML = srcHTML.replace(
-    /\/\/cdn.jsdelivr.net\/npm\/docsify@4\//g,
-    '/'
-  );
+  const outPath = path.resolve(__dirname, '..');
+  const outHTML = srcHTML
+    // Replace CDN URLs with local paths
+    .replace(/\/\/cdn.jsdelivr.net\/npm\/docsify@4\//g, '/')
+    // Enable basePath to simulate /docs as root
+    .replace(/(\/\/ )(basePath:)/, '$2');
 
   fs.writeFileSync(path.resolve(outPath, file), outHTML);
 }
