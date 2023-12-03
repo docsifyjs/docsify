34477d84f318dcafc349a7e13e52a7303e6aaa3b
John Hildenbiddle <john@hildenbiddle.com>
2023-12-03 09:51:24 -0600

    Preview fix test

---
+++ build/html.js
@@ -0,0 +1,23 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Preview
+// =============================================================================
+function generatePreview() {
+  const file = 'index.html';
+  const srcPath = path.resolve(__dirname, '..', 'docs');
+  const srcHTML = fs.readFileSync(path.resolve(srcPath, file), 'utf8');
+  const outPath = path.resolve(__dirname, '..', '.vercel', 'output', 'static');
+  const outHTML = srcHTML.replace(
+    /\/\/cdn.jsdelivr.net\/npm\/docsify@4\//g,
+    '/'
+  );
+
+  fs.writeFileSync(path.resolve(outPath, file), outHTML);
+}
+
+generatePreview();
