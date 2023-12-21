acd69cdaef8396d111a34b36153cd120502a192c
John Hildenbiddle <john@hildenbiddle.com>
2023-12-21 14:24:45 -0600

    Fix console output

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ import { devConfig, prodConfig } from './server.configs.js';
 const bsServer = create();
 const args = process.argv.slice(2);
 const config = args.includes('--dev') ? devConfig : prodConfig;
+const configName = config === devConfig ? 'development' : 'production';
 const isWatch = Boolean(config.files) && config.watch !== false;
-const urlType = configName === 'prod' ? 'CDN' : 'local';
+const urlType = config === devConfig ? 'local' : 'CDN';
 
 // prettier-ignore
 console.log(`\nStarting ${configName} server (${urlType} URLs, watch: ${isWatch})\n`);
