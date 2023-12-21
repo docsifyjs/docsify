5a0a2897f648d989d6766758a91163f55b6e387c
John Hildenbiddle <john@hildenbiddle.com>
2023-12-21 13:26:56 -0600

    Update server start console message

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const args = process.argv.slice(2);
 const configName =
   Object.keys(serverConfigs).find(name => args.includes(`--${name}`)) || 'prod';
 const settings = serverConfigs[configName];
+const isWatch = Boolean(settings.files) && settings.watch !== false;
+const urlType = configName === 'prod' ? 'CDN' : 'local';
 
 // prettier-ignore
-console.log(`\nStarting ${configName} server (watch: ${Boolean(settings.files)})\n`);
+console.log(`\nStarting ${configName} server (${urlType} URLs, watch: ${isWatch})\n`);
 
 bsServer.init(settings);
