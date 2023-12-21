47656e102a6566e6c6429a13aae4be9b45fe9271
John Hildenbiddle <john@hildenbiddle.com>
2023-12-21 13:56:06 -0600

    Fix for Vercal “unsupported modules” error

--- middleware.js
+++ middleware.js
@@ -1,10 +1,33 @@
-import rewriteRules from './rewriterules.config.js';
-
 // Exports
 // =============================================================================
 export const config = {
   matcher: ['/preview/(index.html)?'],
 };
+
+// Rewrite rules shared with local server configurations
+export const rewriteRules = [
+  // Replace CDN URLs with local paths
+  {
+    match: /https?.*\/CHANGELOG.md/g,
+    replace: '/CHANGELOG.md',
+  },
+  {
+    // CDN versioned default
+    // Ex1: //cdn.com/package-name
+    // Ex2: http://cdn.com/package-name@1.0.0
+    // Ex3: https://cdn.com/package-name@latest
+    match: /(?:https?:)*\/\/.*cdn.*docsify[@\d.latest]*(?=["'])/g,
+    replace: '/lib/docsify.min.js',
+  },
+  {
+    // CDN paths to local paths
+    // Ex1: //cdn.com/package-name/path/file.js => /path/file.js
+    // Ex2: http://cdn.com/package-name@1.0.0/dist/file.js => /dist/file.js
+    // Ex3: https://cdn.com/package-name@latest/dist/file.js => /dist/file.js
+    match: /(?:https?:)*\/\/.*cdn.*docsify[@\d.latest]*\/(?:lib\/)/g,
+    replace: '/lib/',
+  },
+];
 
 // Serve virtual /preview/index.html
 // Note: See vercel.json for preview routing configuration
--- server.configs.js
+++ server.configs.js
@@ -1,5 +1,6 @@
 import * as path from 'node:path';
 import * as url from 'node:url';
+import { rewriteRules } from './middleware.js';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,29 +23,7 @@ const dev = {
   ...prod,
   files: ['CHANGELOG.md', 'docs/**/*', 'lib/**/*'],
   port: 3000,
-  rewriteRules: [
-    // Replace CDN URLs with local paths
-    {
-      match: /https?.*\/CHANGELOG.md/g,
-      replace: '/CHANGELOG.md',
-    },
-    {
-      // CDN versioned default
-      // Ex1: //cdn.com/package-name
-      // Ex2: http://cdn.com/package-name@1.0.0
-      // Ex3: https://cdn.com/package-name@latest
-      match: /(?:https?:)*\/\/.*cdn.*docsify[@\d.latest]*(?=["'])/g,
-      replace: '/lib/docsify.min.js',
-    },
-    {
-      // CDN paths to local paths
-      // Ex1: //cdn.com/package-name/path/file.js => /path/file.js
-      // Ex2: http://cdn.com/package-name@1.0.0/dist/file.js => /dist/file.js
-      // Ex3: https://cdn.com/package-name@latest/dist/file.js => /dist/file.js
-      match: /(?:https?:)*\/\/.*cdn.*docsify[@\d.latest]*\/(?:lib\/)/g,
-      replace: '/lib/',
-    },
-  ],
+  rewriteRules,
   server: {
     ...prod.server,
     routes: {
