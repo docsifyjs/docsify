5840fc49265d614586147e1b3de9aefb501c53d2
koy <koy@ko8e24.top>
2024-07-20 18:24:09 +0800

    chore: fix ci on jest

--- test/integration/example.test.js
+++ test/integration/example.test.js
@@ -20,7 +20,7 @@ describe('Creating a Docsify site (integration tests in Jest)', function () {
     const docsifyInitConfig = {
       config: {
         name: 'Docsify Name',
-        // themeColor: 'red',
+        themeColor: 'red',
       },
       markdown: {
         coverpage: `
@@ -76,7 +76,7 @@ describe('Creating a Docsify site (integration tests in Jest)', function () {
 
     // Verify config options
     expect(typeof window.$docsify).toBe('object');
-    // expect(window.$docsify).toHaveProperty('themeColor', 'red');
+    expect(window.$docsify).toHaveProperty('themeColor', 'red');
     expect(document.querySelector('.app-name').textContent).toContain(
       'Docsify Name',
     );
