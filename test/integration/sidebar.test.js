a6fb2dd3eab4c3360b37e5c94e20dc4b5068068d
koy <koy@ko8e24.top>
2024-07-21 20:59:50 +0800

    update: replace to add no sidebar config test in sidebar.test.js

--- test/integration/sidebar.test.js
+++ test/integration/sidebar.test.js
@@ -2,17 +2,74 @@ import docsifyInit from '../helpers/docsify-init.js';
 import { expect } from '@playwright/test';
 
 describe('Test sidebar render toc structure', function () {
-  test('Render sidebar with loadSidebar=true and the _sidebar.md file', async () => {
+  // test('Render sidebar with loadSidebar=true and the _sidebar.md file', async () => {
+  //   await docsifyInit({
+  //     config: {
+  //       loadSidebar: '_sidebar.md',
+  //     },
+  //     markdown: {
+  //       homepage: '# Hello World',
+  //       sidebar: `
+  //       - Getting started
+  //           - [Level1](QuickStart.md)
+  //               - [Level2](QuickStart2.md)
+  //       `,
+  //     },
+  //     waitForSelector: '.sidebar-nav > ul',
+  //   });
+
+  //   const sidebarElm = document.querySelector('.sidebar');
+  //   /**
+  //    * Expected render result
+  //    * ==========================
+  //    *<ul>
+  //    *   <li>
+  //    *       Getting started
+  //    *       <ul>
+  //    *           <li>
+  //    *               <a href="#/QuickStart" title="Level1">Level1</a>
+  //    *               <ul>
+  //    *               <li><a href="#/QuickStart2" title="Level2">Level2</a></li>
+  //    *               </ul>
+  //    *           </li>
+  //    *       </ul>
+  //    *   </li>
+  //    * </ul>
+  //    */
+
+  //   const GettingStarted = document.querySelector('.sidebar-nav > ul > li');
+  //   const level1_Elm = document.querySelector(
+  //     '.sidebar-nav > ul > li > ul> li',
+  //   );
+  //   const level1_A_tag = level1_Elm.querySelector(' a');
+
+  //   const level2_Elm = level1_Elm.querySelector(' ul > li ');
+
+  //   const level2_A_tag = level2_Elm.querySelector(' a');
+
+  //   expect(sidebarElm).not.toBeNull();
+  //   expect(GettingStarted).not.toBeNull();
+  //   expect(level1_Elm).not.toBeNull();
+  //   expect(level1_A_tag).not.toBeNull();
+  //   expect(level2_Elm).not.toBeNull();
+  //   expect(level2_A_tag).not.toBeNull();
+  //   expect(level1_A_tag.textContent).toContain('Level1');
+  //   expect(level2_A_tag.textContent).toContain('Level2');
+  // });
+
+  test('Render sidebar with loadSidebar=false should be same to loadSidebar=true sidebar structure', async () => {
     await docsifyInit({
       config: {
-        loadSidebar: '_sidebar.md',
+        loadSidebar: false,
       },
       markdown: {
-        homepage: '# Hello World',
-        sidebar: `
-        - Getting started
-            - [Level1](QuickStart.md)
-                - [Level2](QuickStart2.md)
+        homepage: `
+        # Getting started 
+           some thing
+        ## Level1 
+            foo
+        ### Level2 
+            bar
         `,
       },
       waitForSelector: '.sidebar-nav > ul',
