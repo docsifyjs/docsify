a042acada5fb725d7ded4467c89f9469a7dc0b89
Joe Pea <joe@lume.io>
2025-11-16 23:55:34 -0800

    remove deprecation messages from global API

--- src/core/global-api.js
+++ src/core/global-api.js
@@ -6,17 +6,7 @@ import { Compiler } from './render/compiler.js';
 import { slugify } from './render/slugify.js';
 import { get } from './util/ajax.js';
 
-/**
- * @deprecated This is deprecated, kept for backwards compatibility. It will be
- * Removed in a major release. Get everything from the DOCSIFY global when using
- * the global build, but we highly recommend to import Docsify using standard
- * import syntax.
- */
 export default function initGlobalAPI() {
-  console.warn(
-    'DEPRECATION: The Docsify global script is deprecated. See the latest getting started docs. https://docsify.js.org/#/quickstart',
-  );
-
   window.Docsify = {
     util,
     dom,
