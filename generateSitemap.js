5096428ebb587f443d6f590fc1096a4a387bcd7a
ThirstyWater <112556376+thirstywaterx@users.noreply.github.com>
2026-04-06 17:53:56 +0800

    Update generateSitemap.js

--- generateSitemap.js
+++ generateSitemap.js
@@ -12,6 +12,7 @@ function walk(dir) {
   for (const file of fs.readdirSync(dir)) {
     const fullPath = path.join(dir, file);
     const stat = fs.statSync(fullPath);
+    
     if (stat.isDirectory()) {
       files = files.concat(walk(fullPath));
     } else if (file.endsWith('.md')) {
@@ -22,25 +23,32 @@ function walk(dir) {
   return files;
 }
 
-const files = walk(docsDir);
+const allFiles = walk(docsDir);
 const now = new Date().toISOString().split('T')[0];
 
-const urls = files.map(f => {
-  let urlPath = f.replace(/\.md$/, '');
-  if (urlPath.toLowerCase() === 'readme') urlPath = '';
-  
-  return `
+const urls = allFiles
+  .filter(f => !f.startsWith('_')) 
+  .map(f => {
+    let urlPath = f.replace(/\.md$/, '');
+    if (urlPath.toLowerCase() === 'readme') urlPath = '';
+    
+    return `
   <url>
     <loc>${siteUrl}${urlPath.replace(/ /g, '%20')}</loc>
     <lastmod>${now}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>`;
-}).join('');
+  }).join('');
 
 const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>`;
 
-fs.writeFileSync(sitemapPath, sitemapXml);
+try {
+  fs.writeFileSync(sitemapPath, sitemapXml);
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
