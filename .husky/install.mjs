d2427b8c10945cd4ed6d73c06e795dbb85113808
Luffy <lufei@apache.org>
2026-08-21 16:26:23 +0800

    fix: update postinstall script and add install.mjs for Husky setup

---
+++ .husky/install.mjs
@@ -0,0 +1,6 @@
+// Skip Husky install in production and CI
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+  process.exit(0);
+}
+const husky = (await import('husky')).default;
+console.log(husky());
